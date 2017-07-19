1537c74fb88f66b58fb44a092a5a955e79c5ecf2
diego <dhurtadorosales@gmail.com>
2017-07-19 14:19:56 +0200

    Editar un usuario

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Services\Helpers;
+use AppBundle\Services\JwtAuth;
 use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -40,6 +41,7 @@ class UserController extends Controller
             $validateEmail = $this->get('validator')->validate($email, $emailConstraint);
 
             if ($email != null && count($validateEmail) == 0 && $password != null && $name != null && $surname != null) {
+                //NUEVO USUARIO
                 $user = new User();
                 $user
                     ->setCreatedAt($createdAt)
@@ -80,67 +82,87 @@ class UserController extends Controller
     }
 
     /**
-     * @Route("/user/new", name="user_new")
+     * @Route("/user/edit", name="user_edit")
      */
-    public function newAction(Request $request)
+    public function editAction(Request $request)
     {
         $helpers = $this->get(Helpers::class);
-        $json = $request->get('json', null);
-        $params = json_decode($json);
+        $jwtAuth = $this->get(JwtAuth::class);
+        $token = $request->get('authorization', null);
+        $authCheck = $jwtAuth->checkToken($token);
 
-        $data = [
-            'status' => 'error',
-            'code' => 400,
-            'message' => 'User not created'
-        ];
+        if ($authCheck) {
 
-        if ($json != null) {
-            $createdAt = new \DateTime('now');
-            $role = 'user';
-            $email = (isset($params->email)) ? $params->email : null;
-            $name = (isset($params->name)) ? $params->name : null;
-            $surname = (isset($params->surname)) ? $params->surname : null;
-            $password = (isset($params->password)) ? $params->password : null;
+            //OBTENCIÓN DEL USUARIO LOGUEADO
+            /** @var EntityManager $em */
+            $em = $this->getDoctrine()->getManager();
+            $identity = $jwtAuth->checkToken($token, true);
+            $user = $em->getRepository('BackendBundle:User')->findOneBy([
+                'id' => $identity->sub
+            ]);
 
-            $emailConstraint = new Assert\Email();
-            $emailConstraint->message = 'This email is not valid';
-            $validateEmail = $this->get('validator')->validate($email, $emailConstraint);
+            //RECOGER LOS DATOS
+            $json = $request->get('json', null);
+            $params = json_decode($json);
 
-            if ($email != null && count($validateEmail) == 0 && $password != null && $name != null && $surname != null) {
-                $user = new User();
-                $user
-                    ->setCreatedAt($createdAt)
-                    ->setRole($role)
-                    ->setEmail($email)
-                    ->setName($name)
-                    ->setSurname($surname);
+            //ERROR POR DEFECTO
+            $data = [
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'User not updated'
+            ];
 
-                /** @var EntityManager $em */
-                $em = $this->getDoctrine()->getManager();
+            if ($json != null) {
+                //$createdAt = new \DateTime('now');
+                $role = 'user';
+                $email = (isset($params->email)) ? $params->email : null;
+                $name = (isset($params->name)) ? $params->name : null;
+                $surname = (isset($params->surname)) ? $params->surname : null;
+                $password = (isset($params->password)) ? $params->password : null;
 
-                //COMPROBAMOS SI EXISTE
-                $issetUser = $em->getRepository('BackendBundle:User')->findBy([
-                    'email' => $email
-                ]);
-                if (count($issetUser) == 0) {
-                    $em->persist($user);
-                    $em->flush();
+                $emailConstraint = new Assert\Email();
+                $emailConstraint->message = 'This email is not valid';
+                $validateEmail = $this->get('validator')->validate($email, $emailConstraint);
 
-                    $data = [
-                        'status' => 'success',
-                        'code' => 200,
-                        'message' => 'User created',
-                        'user' => $user
-                    ];
-                }
-                else {
-                    $data = [
-                        'status' => 'error',
-                        'code' => 400,
-                        'message' => 'User not created, duplicated'
-                    ];
+                if ($email != null && count($validateEmail) == 0 && $password != null && $name != null && $surname != null) {
+                    $user
+                        //->setCreatedAt($createdAt)
+                        ->setRole($role)
+                        ->setEmail($email)
+                        ->setName($name)
+                        ->setSurname($surname);
+
+                    //COMPROBAMOS SI EXISTE
+                    $issetUser = $em->getRepository('BackendBundle:User')->findBy([
+                        'email' => $email
+                    ]);
+                    if (count($issetUser) == 0 || $identity->email == $email) {
+                        $em->persist($user);
+                        $em->flush();
+
+                        $data = [
+                            'status' => 'success',
+                            'code' => 200,
+                            'message' => 'User updated',
+                            'user' => $user
+                        ];
+                    }
+                    else {
+                        $data = [
+                            'status' => 'error',
+                            'code' => 400,
+                            'message' => 'User not updated, duplicated'
+                        ];
+                    }
                 }
             }
+        }
+        else {
+            $data = [
+                'status' => 'error',
+                'code' => 400,
+                'message' => 'Authorization not valid'
+            ];
         }
 
         return $helpers->json($data);
